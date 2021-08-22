ec8641e8543abf688cfe95f48d24b2ae73f7173f
TebooNok <450015317@qq.com>
2021-08-22 17:16:54 +0800

    Change from another developer

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ public class Main{
         System.out.println("Hello world");
 
         System.out.println("Just a little change");
+
+        System.out.println("More change from another account");
     }
 }
