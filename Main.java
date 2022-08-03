f54778b772a125deb36283bf184d73e12ec806b5
TebooNok <450015317@qq.com>
2022-08-03 16:10:22 +0800

    I remove some print command, and add an exit.

--- Main.java
+++ Main.java
@@ -2,9 +2,8 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        System.out.println("Just a little change");
 
         // According to review comments, add a notation
-        System.out.println("More change from another account");
+        exit();
     }
 }
